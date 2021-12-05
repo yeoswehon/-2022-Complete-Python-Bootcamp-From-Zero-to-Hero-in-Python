188554f20ff3180a56b81472594434d5df9c2db3
ClaXeR <85326512+yeoswehon@users.noreply.github.com>
2021-12-05 17:37:24 +0800

    Create dictionary.py

---
+++ advancedpython/dictionary.py
@@ -0,0 +1,9 @@
+prices_lookup = {'apple':2.99, 'oranges':1.99, 'milks': '5.80'}
+
+print(prices_lookup['apple'])
+
+d = {'k1':123, 'k2':[0,1,2], 'k3': {'insideKey':100}}
+
+print(d['k2'])
+
+print(d['k3']['insideKey'])
